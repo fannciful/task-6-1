8ca65140933639d57798f254558ce2652c085327
Yuliia <fannciful@MacBook-Air---Yuliia.local>
2024-07-22 20:13:18 +0300

    Add validation

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 'use strict';
-
 function removeCharacters(input, charactersToRemove) {
-    let result = '';
-
-    for (let char of input) {
-        if (char === ' ' || !charactersToRemove.includes(char)) {
-            result += char;
-        }
-    }
-    return result;
+    const inputArr = input.split('');
+    const resultArr = inputArr.filter(char => !charactersToRemove.includes(char));
+    return resultArr.join('');
 }
 
+const userInput = prompt('Введіть вихідний рядок');
 
-const input = prompt('Введіть вихідний рядок');
-const charactersToRemove = prompt('Введіть символи для видалення').split('');
-const result = removeCharacters(input, charactersToRemove);
-alert(`Результат: ${result}`);
-console.log(result);
+if (userInput === null) {
+    alert('You canceled!');
+} else if (!userInput.trim()) {
+    alert('You have entered nothing!');
+} else {
+    const charactersToRemoveInput = prompt('Введіть символи для видалення');
+    
+    if (charactersToRemoveInput === null || !charactersToRemoveInput.trim()) {
+        alert('No characters to remove!');
+    } else {
+        const charactersToRemove = charactersToRemoveInput.split('').filter(char => char.trim() !== '');
+
+        const result = removeCharacters(userInput, charactersToRemove);
+        alert(`Результат: ${result}`);
+        console.log(result);
+    }
+}
